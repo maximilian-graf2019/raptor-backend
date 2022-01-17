05cd6e30d6fc0e43d5c685dae1e1db3ca53b25bb
maximilian-graf2019 <maxgraf27@gmail.com>
2022-01-17 14:04:11 +0100

    improved Readme, heroku deployment

---
+++ README.md
@@ -0,0 +1,3 @@
+### Hier findet sich das Backend von raptor trails
+
+Die Platform raptor trails findet ihr [hier](www.raptor-trails.com).
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,17 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
 import router from "./routes.ts";
+import { serve } from "https://deno.land/std@0.57.0/http/server.ts";
+import { parse } from 'https://deno.land/std/flags/mod.ts';
+
+const { args } = Deno;
 const PORT = 4000;
+const argPort = parse(args).port;
+
+const s = serve({ port: argPort ? Number(argPort) : PORT });
+
+for await (const req of s) {
+    req.respond({ body: "raptor trails rocks\n" });
+}
 
 const app = new Application();
 
